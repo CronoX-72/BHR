0b4779dac086937e9006bcf98fe21aa7e41c808d
CronoX <prdlc@outlook.com>
2021-07-04 18:26:16 -0500

    cintillo de pie de paguina

--- footer.php
+++ footer.php
@@ -1,4 +1,8 @@
 <?php get_footer(); ?>
+        <footer>
+            <?php get_template_part('template-parts/footer/pie-paguina'); ?>
+            <?php get_template_part('template-parts/footer/cintillo-pie-paguina'); ?>
+        </footer>
     </div>
     <?php wp_footer(); ?>
 </body>
---
+++ template-parts/footer/cintillo-pie-paguina.php
@@ -0,0 +1,9 @@
+<div class="cintillo-pie-paguina">
+    <p>Todos los derechos reservados</p>
+    <?php bloginfo("name"); ?>
+    <?php echo date("Y"); ?>
+    <div class="realizado-por">
+        <p>Tema realizado por</p>
+        <a href="https://twitter.com/CronoX_RdlC" target="_blanck" class="enlace-hover">CronoX RdlC</a>
+    </div>
+</div>
